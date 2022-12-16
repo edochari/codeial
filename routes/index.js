82f8e6ea1f5c0e97998153b43deb42872cd07f3d
edochari <120657291+edochari@users.noreply.github.com>
2022-12-16 20:02:40 +0530

    part 1

--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,9 @@ const express=require('express');
 const router=express.Router();
 const homeController=require('../controllers/home_controller');
 
-router.get('/home',homeController.home);
+router.get('/',homeController.home);
 router.use('/users',require('./users'));
-router.use('/users/',require('./post'));
+router.use('/users',require('./post'));
 
 
 console.log("router is loaded");
